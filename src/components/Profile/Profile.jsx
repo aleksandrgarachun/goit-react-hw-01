f640a22a2233f7eda0521f92f16051a30056b0b4
aleksandrgarachun <115419603+aleksandrgarachun@users.noreply.github.com>
2024-02-21 20:31:58 +0300

    change css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,30 +4,30 @@ import css from './Profile.module.css'
 
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div className={css['profile-section']}>
-      <div className={css['profile-info']}>
+    <div className={css.profileSection}>
+      <div className={css.profileInfo}>
         <img 
         src={image}
         alt="User avatar"
-        className={css['profile-image']} 
+        className={css.profileImage} 
         />
-        <p className={css['profile-name']}>{name}</p>
-        <p className={css['profile-tag']}>@{tag}</p>
-        <p className={css['profile-location']}>{location}</p>
+        <p className={css.profileName}>{name}</p>
+        <p className={css.profileTag}>@{tag}</p>
+        <p className={css.profileLocation}>{location}</p>
       </div>
 
-      <ul className={css['profile-status']}>
-        <li className={css['profile-status-info']}>
-          <span className={css['foll-vie-likes']}>Followers</span>
-          <span className={css['numbers-info']}>{stats.followers}</span>
+      <ul className={css.profileStatus}>
+        <li className={css.profileStatusInfo}>
+          <span className={css.follVieLikes}>Followers</span>
+          <span className={css.numbersInfo}>{stats.followers}</span>
         </li>
-        <li className={css['profile-status-info']}>
-          <span className={css['foll-vie-likes']}>Views</span>
-          <span className={css['numbers-info']}>{stats.views}</span>
+        <li className={css.profileStatusInfo}>
+          <span className={css.follVieLikes}>Views</span>
+          <span className={css.numbersInfo}>{stats.views}</span>
         </li>
-        <li className={css['profile-status-info']}>
-          <span className={css['foll-vie-likes']}>Likes</span>
-          <span className={css['numbers-info']}>{stats.likes}</span>
+        <li className={css.profileStatusInfo}>
+          <span className={css.follVieLikes}>Likes</span>
+          <span className={css.numbersInfo}>{stats.likes}</span>
         </li>
       </ul>
     </div>
